0d9b69da7fc66695a3cce6df3ee676909a303add
Mike <korostelevm@gmail.com>
2023-09-26 11:12:21 -0400

    asdf

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Hello App!</h1>
-    <p>
-      <!-- use the router-link component for navigation. -->
-      <!-- specify the link by passing the `to` prop. -->
-      <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
-      <router-link to="/">Go to Home</router-link>
-      <!-- <router-link to="/posts"></router-link> -->
-      <router-link to="/about">Go to About</router-link>
-    </p>
+      <div class="header">
+        <img alt="Vue logo" src="./assets/logo.png">
+          <h1>Hello App!</h1>
+          <p>
+            <!-- use the router-link component for navigation. -->
+            <!-- specify the link by passing the `to` prop. -->
+            <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
+            <router-link to="/">Go to Home</router-link>
+            <!-- <router-link to="/posts"></router-link> -->
+            <!-- <router-link to="/about">Go to About</router-link> -->
+          </p>    
+      </div>
+    
     <router-view></router-view>
   </div>
 </template>
@@ -29,8 +32,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.header{
+  text-align: center;
 }
 </style>
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,4 +1,14 @@
-<script setup>
+<template>
+  <div>
+    <div v-if="post">
+      <h1>{{post.metadata.title}}</h1>
+      <div class="line-numbers language-markup" v-html="mdToHtml"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+
 import { ref, computed } from "vue";
 import { marked } from "marked";
 import prism from "prismjs";
@@ -15,11 +25,6 @@ import "prismjs/plugins/show-language/prism-show-language.js"; // display the la
 // This is needed for a conflict with other CSS files being used (i.e. Bulma).
 import "prismjs/plugins/custom-class/prism-custom-class";
 prism.plugins.customClass.map({ number: "prism-number", tag: "prism-tag" });
-
-// sample markdown file
-const mdfile = "/sample.md";
-const markDown = ref("");
-
 //marked Options => https://marked.js.org/using_advanced#options
 marked.use({
   highlight: (code, lang) => {
@@ -31,40 +36,37 @@ marked.use({
   },
 });
 
-// read in the md file and apply the highlight style to the Code Block
-const getMarkdownData = async () => {
-    console.log('asdf')
 
-    // let f = require('!raw-loader!./aws-s3-why-sometimes-you-should-press-the-100k-dollar-button.md').default
-  markDown.value = "asdfasdf"
-//   await fetch(mdfile)
-//     .then((response) => response.text())
-//     .then((data) => ( = data));
-  prism.highlightAll(); // perform the highlighting of the Code Blocks
-};
 
-// use the finialized markdown to HTML code in the template
-const mdToHtml = computed(() => {
-  const mdhtml = marked.parse(markDown.value);
-  return mdhtml;
-});
+import axios from 'axios'
+export default {
+  name: 'Post',
+  props: {
+    msg: String
+  },
+  data(){
+    let slug = this.$route.params.id
+    return{
+      post: null,
+      slug: slug,
+      mdToHtml: ""
+    } 
+  },
+  mounted() {   
+    this.getPost()
+    },
+  methods:{
+    async getPost(){
+      let res = await axios.get(`/api/posts/${this.slug}`)
+      this.post = res.data
+      console.log(this.post)  
+      // markDown.value = "asdfasdf"
+      this.mdToHtml =  marked.parse(this.post.body)
+      prism.highlightAll(); // perform the highlighting of the Code Blocks
 
-// call the function to be ran
-// getMarkdownData();
+    }
+    
+  }
+}
 </script>
 
-<template>
-  <div class="content">
-    asdfsdf
-    <!-- Normal Code Blocks -->
-    <!-- <div v-html="mdToHtml"></div> -->
-    <!-- Add numbering to the Code Blocks -->
-    <div class="line-numbers language-markup" v-html="mdToHtml"></div>
-  </div>
-</template>
-
-<style >
-/* Add the prism themes here */
-/* @import "prismjs/themes/prism-coy.css"; */
-
-</style>
